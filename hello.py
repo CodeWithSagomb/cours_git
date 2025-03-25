d0f5b281f00b29f83564593145f98adecdaab8ca
CHRIST sagombaye <mbayechriston@gmail.com>
2025-03-25 15:55:32 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,311 @@
+print("Hello, World!")
+print("bonjour bro, comment ça va?")
+print("je suis un programmeur")
+print("je suis un programmeur")
+print("je suis un etudiant")
+print("je suis un etudiant")
+print("je suis un etudiant tchadien en algerie")
+print("je suis christ")
+a=2
+b=2
+print(a+b)
+
+print("bonjour bro, comment vas tu avec les cours?")
+print("hello world!")
+if 5<6:
+    print("great")
+
+A=10
+B=20
+if B > A:
+    print("B is greater than A")
+
+# this is a comment 
+print("hello bro!")
+
+# this is a comment written in 
+# more than just one line
+# i'll like to say hi christ 
+
+print("awesome bro")
+
+X=5
+y="christ"
+print(X)
+print(y)
+
+X= 6
+X="aaa"
+print(X)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+a=str(5)
+b=int(3)
+c=float(5)
+print(a)
+print(a+b)
+type(a)
+print(type(a))
+print(type(c))
+type(b)
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+n, m, k = "q", "p", "o"
+print(n)
+print(m)
+print(k)
+
+w=x=y=5
+print(w,"",x,"",y)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+Lt= ["a","b","c"]
+x, y, z = Lt
+print(x)
+print(y)
+print(z)
+
+x = "awesome"
+
+def myfunc():
+ print("Python is " + x)
+
+myfunc()
+
+def myFunc():
+    print("Python is an "+ x,"language")
+
+myFunc()
+
+
+name = "christ Sagombaye"
+print(name)
+print(name[0])
+len(name)
+name[15]
+
+for i in name:
+    print(i)
+
+txt = "the best thing in life are free"    
+if "free" in txt:
+    print("yes, 'free' is present")
+
+if "expensive" not in txt:
+    print("expensive is Not present")
+
+b="hello brother, how are you?"
+len(b)
+b[0:11]
+print(b[0:11])
+b[:5]
+print(b[5:])
+
+name.upper()
+name.lower()
+nickname=" AwesomeBro  GREAT  "
+nickname
+N=nickname.strip()
+N
+name=name.replace("christ","Djetam")
+name
+
+print(name+" "+nickname)
+
+age=28
+tt= f"my name is christ,iam {age} "
+print(tt)
+
+print(10>2)
+print(10==11)
+
+
+x=2
+y=20
+if x > y:
+    print(x,"is greater than",y)
+else:
+    print(x, "is not greater than ",y)
+
+a == B
+a != B
+a < b
+a <= B
+a >= B
+
+a = 33
+b = 33
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a is equal to b")    
+
+
+a = 200
+b = 33
+
+if a > b:
+    print("a is greater than b")
+elif a == b:
+    print("a is equal to b")
+else:
+    print(" b is greater than a")        
+
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
+
+a = 33
+b = 200
+
+if b > a:
+  pass
+
+
+i = 1
+while i < 6:
+    print(i)
+    i+=1
+
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+
+for x in range(2,16,2):
+    print(x)
+else:
+    print("Finally finished")    
+
+
+for x in range(2,20):
+    if x == 10: break
+    print(x)
+else:
+    print("Finally finished")        
+
+
+adj=["red","big","tasty"]
+fruit=["apple","banana","cherry"]
+
+for a in adj:
+    for b in fruit:
+        print(a,b)
+
+
+def funct():
+    print("hello from Sago")        
+
+funct()    
+
+def fct2(fnam):
+    fnam=fnam
+    print(fnam + " sagommbaye")
+
+fct2("christ")
+
+def fct3(fname,lname):
+    print(fname,lname)
+
+fct3("christ","sagombaye")
+
+def fct4(pays="tchad"):
+    print("je suis du ",pays)
+
+fct4("cameroun")
+
+def my_funct(food):
+    for x in food:
+        print(x)
+
+fruits = ["apple", "banana", "cherry"]
+ff=[2,3,24,45,100]
+my_funct(fruits)
+my_funct(ff)
+
+def cal(x):
+    return 5*x
+
+cal(5)    
+
+xx=lambda a : a + 10
+xx(12)
+xx(20)
+
+y=lambda a,b: a*b
+y(12,23)
+
+def myfunc(n):
+  return lambda a : a * n
+
+myfunc(2)
+
+def myfunc(n):
+  return lambda a : a * n
+
+
+print(myfunc(2))
+
+################################
+
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+thislist[0]="orange"
+thislist
+thatlist=thislist
+thatlist[0]="apple"
+thatlist
+thislist
+
+####
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print(list1)
+print(list2)
+print(list3)
+
+L=["sago",1996,True,22.50,0,"hello"]
+print(L)
+type(L)
+L1=list(("apple", "banana", "cherry"))
+L1
+print(type(L1))
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-1])
+thislist
+
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+thislist[3:6]
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[-4:-1])
+thislist
+
+import Module
+
+
